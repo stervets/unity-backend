3bf461a475cca9a0bbee97ecd16c36f44973d8ad
Mike Lisov <mike@datapine.com>
2019-11-07 21:58:06 +0300

    female controller

--- app/actors/scripts/CharacterFemale.js
+++ app/actors/scripts/CharacterFemale.js
@@ -1,4 +1,9 @@
 while (true) {
-    move(random(-5, 10), random(-5, 10));
-    wait(500);
+    //move(random(-5, 5), random(-5, 5));
+    //wait(500);
+    turn(0);
+    turn(90);
+    turn(180);
+    turn(0);
+    turn(-90);
 }
--- app/actors/scripts/StageController.js
+++ app/actors/scripts/StageController.js
@@ -1,3 +1,7 @@
-create('CharacterFemale', 'CharacterFemale', 0, 0, 0, 90);
+//create('CharacterFemale', 'CharacterFemale', 0, 0, 0, 90);
 
-create('CharacterFemale', 'CharacterFemale', 0, 5, 0, -90);
+//create('CharacterFemale', 'CharacterFemale', 0, 5, 0, -90);
+
+
+create('CharacterFemale', 'CharacterFemale', 0, 0, 0, -90);
+//create('CharacterFemale', 'CharacterFemale', 0, 0, 0, -90);
