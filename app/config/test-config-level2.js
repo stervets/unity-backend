3c38c5d71881abf9cb03b27fd1a0f90dead06e84
Mike Lisov <mike@datapine.com>
2020-04-18 16:22:46 +0300

    server fixes

--- app/config/test-config-level2.js
+++ app/config/test-config-level2.js
@@ -1,122 +1,123 @@
-var config = {
-    level: 'scifi/level1',
-    desc : 'Pick up card and go to the door',
+var types  = {
+        TANK  : 1,
+        TURRET: 2
+    },
 
-    scripts: [
-        {
-            name   : 'TankController',
-            isAdmin: false,
-            api    : 'Tank',
-            content: `
+    config = {
+        name: 'Level 2',
+        desc : 'Pick up card and go to the door',
+
+        unity: {
+            name    : 'scifi/level1',
+            bundle  : '/o/Unity%2F888283_apple_512x512.png?alt=media&token=2982afd0-7724-4f76-aea3-546f505ffd87',
+            manifest: '/o/Unity%2F888283_apple_512x512.png?alt=media&token=2982afd0-7724-4f76-aea3-15234613',
+        },
+
+        scripts: [
+            {
+                name   : 'TankController',
+                isAdmin: false,
+                api    : 'Tank',
+                content: `
                motor(1,1);
             `
-        }
-    ],
-
-    api: {
-        Entity: {
-            properties: {
-                TANK  : 1,
-                TURRET: 2
             }
-        },
+        ],
 
-        Vehicle: {
-            extends: 'Entity',
-
-            properties: {
-                DIRECTION: {
-                    FRONT: 0,
-                    LEFT : 1,
-                    RIGHT: 2,
-                    BACK : 3
+        api: {
+            Entity: {
+                properties: {
+                    TYPE: types
                 }
             },
 
-            methods: {
-                motor: {
-                    desc  : 'Motor direct control',
-                    params: [
-                        {
-                            name: 'left',
-                            type: 'float',
-                            desc: 'Left motor'
-                        },
-                        {
-                            name: 'right',
-                            type: 'float',
-                            desc: 'Right motor'
-                        }
-                    ]
+            Vehicle: {
+                extends: 'Entity',
+
+                properties: {
+                    DIRECTION: {
+                        FRONT: 0,
+                        LEFT : 1,
+                        RIGHT: 2,
+                        BACK : 3
+                    }
                 },
 
-                move: {
-                    desc  : 'Move vehicle forward or backward a set number of meters. Positive values indicate a forward movement, while negative indicates backward.',
-                    params: [
-                        {
-                            name: 'distance',
-                            type: 'float',
-                            desc: 'Distance to move'
-                        }
-                    ]
-                },
+                methods: {
+                    motor: {
+                        desc  : 'Motor direct control',
+                        params: [
+                            {
+                                name: 'left',
+                                type: 'float',
+                                desc: 'Left motor'
+                            },
+                            {
+                                name: 'right',
+                                type: 'float',
+                                desc: 'Right motor'
+                            }
+                        ]
+                    },
 
-                radar: {
-                    desc  : 'Search an object',
-                    params: [
-                        {
-                            name: 'type',
-                            type: 'int',
-                            desc: 'Object type'
-                        },
-                        {
-                            name   : 'angle',
-                            type   : 'int',
-                            desc   : 'Radar angle',
-                            default: 360
-                        },
-                        {
-                            name   : 'distance',
-                            type   : 'float',
-                            desc   : 'Radar distance',
-                            default: 1000
-                        }
-                    ]
-                },
-            }
-        },
+                    move: {
+                        desc  : 'Move vehicle forward or backward a set number of meters. Positive values indicate a forward movement, while negative indicates backward.',
+                        params: [
+                            {
+                                name: 'distance',
+                                type: 'float',
+                                desc: 'Distance to move'
+                            }
+                        ]
+                    },
 
-        Tank: {
-            public    : true,
-            extends   : 'Vehicle',
-            title     : 'My super tank',
-            properties: {
-                type: config.api.Entity.properties.TANK,
-                color: 'red'
-            },
-
-            methods: {
-                shoot: { //это, допустим, уникальный метод только для танка
-                    desc: 'Motor direct control'
+                    radar: {
+                        desc  : 'Search an object',
+                        params: [
+                            {
+                                name: 'type',
+                                type: 'int',
+                                desc: 'Object type'
+                            },
+                            {
+                                name   : 'angle',
+                                type   : 'int',
+                                desc   : 'Radar angle',
+                                default: 360
+                            },
+                            {
+                                name   : 'distance',
+                                type   : 'float',
+                                desc   : 'Radar distance',
+                                default: 1000
+                            }
+                        ]
+                    },
                 }
             },
 
-            events: {
-                onClick: {
-                    desc: 'On Click'
-                }
-            }
-        },
+            Tank: {
+                isClass   : true,
+                extends   : 'Vehicle',
+                title     : 'My super tank',
+                properties: {
+                    type : types.TANK,
+                    color: 'red'
+                },
 
-        Tank2: {
-            public    : true,
-            extends   : 'Tank',
-            title     : 'My super tank 2',
-            properties: {
-                color: 'green'
+                methods: {
+                    shoot: {
+                        desc: 'Motor direct control'
+                    }
+                },
+
+                events: {
+                    onClick: {
+                        desc: 'On Click'
+                    }
+                }
             }
         }
-    }
-};
+    };
 
 module.exports = config;
